ca2f366506f9024ad4dadac4e6bab19eb8aa95a9
Maksim <max.zenkovich@gmail.com>
2018-03-08 23:23:16 +0300

    add Belarusian language puck

---
+++ src/flipclock/js/lang/be-be.js
@@ -0,0 +1,27 @@
+(function($) {
+
+  /**
+   * FlipClock Belarusian Language Pack
+   *
+   * This class will used to translate tokens into the Belarusian language.
+   *
+   */
+
+  FlipClock.Lang.Belarusian = {
+
+    'years'   : 'гадоў',
+    'months'  : 'месяцаў',
+    'days'    : 'дзён',
+    'hours'   : 'гадзін',
+    'minutes' : 'хвілін',
+    'seconds' : 'секунд'
+
+  };
+
+  /* Create various aliases for convenience */
+
+  FlipClock.Lang['be']      = FlipClock.Lang.Belarusian;
+  FlipClock.Lang['be-be']   = FlipClock.Lang.Belarusian;
+  FlipClock.Lang['Belarusian']  = FlipClock.Lang.Belarusian;
+
+}(jQuery));
